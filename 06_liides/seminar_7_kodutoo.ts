d5649d11781f63971a7518190292280871cf7b9e
Merilyn Litvinov <114921922+merilynl@users.noreply.github.com>
2023-03-20 13:04:25 +0200

    Add files via upload

---
+++ 06_liides/seminar_7_kodutoo.ts
@@ -0,0 +1,41 @@
+interface EmployeeInterface{
+    name: string;
+    department: string;
+    IDCode: number;
+    calculateHoursWorked(hours:Array<number>): number;
+    calculateSalary(hours: number, payRate: number): number;
+    
+}
+
+class Employee implements EmployeeInterface{
+    name: string;
+    department: string;
+    IDCode: number;
+
+    constructor(n: string, dept: string, code: number) { 
+        this.name = n;
+        this.department = dept;
+        this.IDCode = code;
+    }
+
+    calculateHoursWorked(h: number[]): number {
+        let hoursWorked = 0;
+        if(h.length > 0) {
+            for(let i = 0; i < h.length; i++){
+                hoursWorked += h[i];  
+            }
+        }
+        return hoursWorked;
+    }
+
+    calculateSalary(h: number, rate: number): number {
+        let salary = h*rate;
+        return salary;
+    }
+}
+
+let accountant: EmployeeInterface = new Employee("John Doe", "Finance", 1);
+let accountantHoursWorked: number = accountant.calculateHoursWorked([8, 7, 6, 5, 8, 3, 2, 8, 4]);
+let accountantSalary: number = accountant.calculateSalary(accountantHoursWorked, 120);
+console.log("Raamatupidaja palk on " + accountantSalary);
+console.log("Raamatupidaja tootas " + accountantHoursWorked + " tundi");
